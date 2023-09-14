e2cd1615c1d181d1501d6f01ea6c896f20218e14
Goran Brbot <goran.brbot@mail.com>
2023-09-14 20:51:21 +0200

    prebačeno u funkciju uz hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,38 @@
-import './App.css'
-import { Component } from 'react'
-import { UserClass, UserFunction, UserChildren } from './user'
+import "./App.css";
+import { Component, useState } from "react";
+import { UserClass, UserFunction, UserChildren } from "./user";
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-  
-    this.state = {
-      users: [
-        { name: 'Ivan', years: 30 },
-        { name: 'Marko', years: 35 },
-        { name: 'Ana', years: 25 },
-      ],
-      text: "Neki tekst"
-    }
-  }
+function App() {
+  const initialUsers = [
+    { name: "Ivan", years: 30 },
+    { name: "Marko", years: 35 },
+    { name: "Ana", years: 25 },
+  ];
 
-  uvecajGodine = () => {
-    const newUsers = this.state.users.map(user => {
-      return {...user, years: user.years + 1 }
-    })
-    this.setState({users: newUsers})
-  }
+  const [users, setUsers] = useState(initialUsers);
 
-  render() {
-    const {users, text} = this.state
-    return (
-      <>
-        <h1>State</h1>
-        <UserClass name={users[0].name} years={users[0].years} />
-        <UserFunction name={users[1].name} years={users[1].years} />
-        <UserChildren name={users[2].name} years={users[2].years}>
-          a hobi mi je programiranje...
-          <p>{text}</p>
-        </UserChildren>
-        <button onClick={this.uvecajGodine}>Uvecaj godine</button>
-      </>
-    )
-  }
+  const tekst = "Proizvoljan tekst";
+
+  const uvecajGodine = () => {
+    setUsers((prevState) => {
+      const updatedUsers = prevState.map((user) => {
+        return { ...user, years: user.years + 1 };
+      });
+      return updatedUsers;
+    });
+  };
+
+  return (
+    <>
+      <h1>State</h1>
+      <UserClass name={users[0].name} years={users[0].years} />
+      <UserFunction name={users[1].name} years={users[1].years} />
+      <UserChildren name={users[2].name} years={users[2].years}>
+        {tekst}
+      </UserChildren>
+      <button onClick={uvecajGodine}>Uvecaj godine</button>
+    </>
+  );
 }
 
-export default App
+export default App;
